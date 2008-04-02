45f7d5a3e617a8bda3eb4e087aefb739b540504a
bms@anglepoise-fxp0.lon.incunabulum.net <bms@anglepoise-fxp0.lon.incunabulum.net>
2008-04-02 14:36:37 +0100

    Use hexdumper from a class file instead of embedding it in this file.

--- tests/tlvtest.py
+++ tests/tlvtest.py
@@ -36,6 +36,7 @@
 
 import unittest
 import sys
+import hexdumper
 
 if __name__ == '__main__':
 
@@ -44,23 +45,6 @@ if __name__ == '__main__':
         sys.argv.remove("-l") # Needed because unittest has issues
                               # with extra arguments.
     import pcs
-
-# This hack by: Raymond Hettinger
-class hexdumper:
-    """Given a byte array, turn it into a string. hex bytes to stdout."""
-    def __init__(self):
-	self.FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' \
-						    for x in range(256)])
-
-    def dump(self, src, length=8):
-	result=[]
-	for i in xrange(0, len(src), length):
-	    s = src[i:i+length]
-	    hexa = ' '.join(["%02X"%ord(x) for x in s])
-	    printable = s.translate(self.FILTER)
-	    result.append("%04X   %-*s   %s\n" % \
-			  (i, length*3, hexa, printable))
-	return ''.join(result)
 
 class testPacket(pcs.Packet):
     """Define a packet containing a TLV field for use in testing."""
