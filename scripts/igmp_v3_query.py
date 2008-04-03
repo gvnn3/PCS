3886ae3608f91d4cf0fd26508e71b00a6b336065
bms@anglepoise-fxp0.lon.incunabulum.net <bms@anglepoise-fxp0.lon.incunabulum.net>
2008-04-03 02:19:01 +0100

    PCS is now capable of sending full IGMPv3 queries!

--- scripts/igmp_v3_query.py
+++ scripts/igmp_v3_query.py
@@ -8,18 +8,14 @@ from pcs.packets.payload import *
 from pcs import *
 from time import sleep
 
-# Send an IGMPv3 general or group-specific query.
-# TODO: Get source-specific queries working.
+# Send an IGMPv3 query. General, group-specific, or
+# group-and-source-specific queries are supported.
 
 def main():
 
     from optparse import OptionParser
 
     parser = OptionParser()
-
-    parser.add_option("-s", "--ip_source",
-                      dest="ip_source", default=None,
-                      help="The IP source address.")
 
     parser.add_option("-I", "--ether_iface",
                       dest="ether_iface", default=None,
@@ -29,38 +25,54 @@ def main():
                       dest="ether_source", default=None,
                       help="The host Ethernet source address.")
 
-    parser.add_option("-c", "--count",
-                      dest="count", default=None,
-                      help="Stop after receiving at least count responses.")
+    parser.add_option("-s", "--ip_source",
+                      dest="ip_source", default=None,
+                      help="The IP source address.")
 
     parser.add_option("-G", "--igmp_group",
                       dest="igmp_group", default=None,
                       help="The IPv4 group to query.")
 
-    parser.add_option("-M", "--maxresp",
-                      dest="maxresp", default=None,
-                      help="The maximum time for end-stations to respond (in seconds).")
-
     parser.add_option("-S", "--igmp_sources",
                       dest="igmp_sources", default=None,
-                      help="Comma-delimited list of IPv4 sources for a group-and-source specific query.")
+                      help="Comma-delimited list of IPv4 sources for "
+			   "a group-and-source specific query.")
+
+    parser.add_option("-M", "--igmp_maxresp",
+                      dest="igmp_maxresp", default=None,
+                      help="The maximum time for end-stations to "
+		           "respond (in seconds).")
+
+    parser.add_option("-c", "--count",
+                      dest="count", default=None,
+                      help="Stop after receiving at least count responses.")
 
     (options, args) = parser.parse_args()
+
+    if options.ether_iface is None or \
+       options.ether_source is None or \
+       options.ip_source is None or \
+       options.count is None:
+	print "Non-optional argument missing."
+	return
+
+    maxresp = 3 * 10
+    if options.igmp_maxresp is not None:
+        maxresp = int(options.igmp_maxresp) * 10    # in units of deciseconds
 
     #
     # Parse source list for a GSR query.
     #
-    igmp_sources = None
+    igmp_sources = []
     if options.igmp_sources is not None:
 	if options.igmp_group is None:
 	    raise "A group must be specified for a GSR query."
 	else:
-	    igmp_sources = []
 	    for source in options.igmp_sources.split(','):
 		igmp_sources.append(inet_atol(source))
 	    if len(igmp_sources) == 0:
 	 	raise "Error parsing source list."
-    
+
     # Set up the vanilla packet
     ether = ethernet()
     ether.type = 0x0800
@@ -83,25 +95,29 @@ def main():
     ip.protocol = IPPROTO_IGMP
     ip.src = inet_atol(options.ip_source)
 
-    igmp = igmpv3_query(igmp_sources)
-    igmp.maxresp = int(options.maxresp) * 10       # in units of deciseconds
+    igmp = igmpv3_query()
+    igmp.maxresp = maxresp
     igmp.qrv = 2		    # SHOULD NOT be 1, MUST NOT be 0
     igmp.qqic = 10		    # I query every 10 seconds
 
     if options.igmp_group is None:
         # General query.
-        ip.dst = inet_atol("224.0.0.1")
+        ip.dst = INADDR_ALLHOSTS_GROUP
         igmp.group = 0
     else:
         # Group-specific query, possibly with sources.
         ip.dst = inet_atol(options.igmp_group)
         igmp.group = ip.dst
 
+    for src in igmp_sources:
+        igmp.sources.append(pcs.Field("", 32, default = src))
+    igmp.nsrc = len(igmp_sources)
+
     igmp_packet = Chain([igmp])
     igmp.checksum = igmp_packet.calc_checksum()
 
     ip.length = len(ip.bytes) + len(igmp.bytes)
-    ip.checksum = ip.calc_checksum()
+    ip.checksum = ip.cksum()
 
     packet = Chain([ether, ip, igmp])
     packet.encode()
