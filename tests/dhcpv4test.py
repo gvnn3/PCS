e26a008a4b178ee14158b0400c71f2e066752cad
bms@empiric.local <bms@empiric.local>
2008-04-17 13:30:14 +0100

    dhcp v4 test placeholder

---
+++ tests/dhcpv4test.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2008, Bruce M. Simpson
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# Neither the name of the authors nor the names of contributors may be
+# used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# File: $Id$
+#
+# Author: Bruce M. Simpson
+#
+# Description: This module performs a self test on a DHCPv4 packet.
+
+import unittest
+
+import sys
+
+if __name__ == '__main__':
+
+    if "-l" in sys.argv:
+        sys.path.insert(0, "../") # Look locally first
+        sys.argv.remove("-l") # Needed because unittest has issues
+                              # with extra arguments.
+
+    from pcs.packets.udp import *
+    from pcs.packets.dhcpv4 import *
+    #from pcs.packets.dhcpv4 import BOOTREQUEST # can't. why?
+
+class bootpTestCase(unittest.TestCase):
+    def test_dhcpv4(self):
+        p = dhcpv4()
+        assert (p != None)
+
+	#p.op = pcs.packets.dhcpv4.BOOTREQUEST
+	p.op = dhcpv4.BOOTREQUEST
+
+if __name__ == '__main__':
+    unittest.main()
