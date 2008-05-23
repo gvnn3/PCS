5fabfc2ce0ec9a8184882313c69b75ca1a259cfd
bms@empiric.lon.incunabulum.net <bms@empiric.lon.incunabulum.net>
2008-05-23 05:44:58 +0100

    add basic vlan support

---
+++ pcs/packets/vlan.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2008, Bruce M. Simpson.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# Neither the name of the author nor the names of other
+# contributors may be used to endorse or promote products derived from 
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# File: $Id$
+#
+# Author: Bruce M. Simpson
+#
+# Description: Classes which describe IEEE 802.1q VLAN headers.
+#
+
+import inspect
+import struct
+import time
+
+import pcs
+import pcs.packets.ethernet_map
+import pcs.packets.payload
+
+class vlan(pcs.Packet):
+    """IEEE 802.1q VLAN header"""
+
+    _layout = pcs.Layout()
+    _map = ethernet_map.map
+
+    def __init__(self, bytes = None, timestamp = None, **kv):
+        type = pcs.Field("type", 16)
+        p = pcs.Field("p", 3)
+        cfi = pcs.Field("cfi", 1)		# Canonical MAC
+        vlan = pcs.Field("vlan", 12)
+
+        pcs.Packet.__init__(self, [ type, p, cfi, vlan ], bytes = bytes, **kv)
+        self.description = inspect.getdoc(self)
+
+        if timestamp is None:
+            self.timestamp = time.time()
+        else:
+            self.timestamp = timestamp
+
+        if bytes is not None:
+            offset = self.sizeof()
+            curr = offset
+            remaining = len(bytes) - offset
+            self.data = self.next(bytes[curr:remaining], timestamp=timestamp)
+            if bytes is not None:
+                self.data = payload.payload(bytes[curr:remaining], \
+                                            timestamp = timestamp)
+        else:
+            self.data = None
