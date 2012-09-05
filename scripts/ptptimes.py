9ac73296cbd9acdc63912e6d43f7b4f3c7b72d88
George V. Neville-Neil <gnn@neville-neil.com>
2012-09-05 17:48:37 -0400

    Add natural, i.e. human, timestamps to the output.  We can't use -h with option parser as it's reserved to the help.

--- scripts/ptptimes.py
+++ scripts/ptptimes.py
@@ -39,6 +39,8 @@ from pcs.packets.ipv4 import ipv4
 from pcs.packets.udpv4 import udpv4
 import pcs
 
+import datetime
+
 def main():
 
     from optparse import OptionParser
@@ -49,7 +51,8 @@ def main():
                       help="File to read from.")
 
     parser.add_option("-n", "--natural",
-                      dest="human", default=False,
+                      action="store_true",
+                      dest="natural", default=False,
                       help="human readable time.")
 
     (options, args) = parser.parse_args()
@@ -64,6 +67,14 @@ def main():
         if packet.data.data == None:
             continue
         if packet.data.data.data != None:
+            if (options.natural == True):
+                ts = datetime.datetime.fromtimestamp(packet.data.data.timestamp)
+                ms = ts.microsecond / 1000
+                msecond = ts.strftime("%H:%M:%S")
+                msecond += (".%d") % ms
+                print msecond
+            else:
+                print packet.data.data.timestamp
             print packet.data.data.data
         if packet.data.data.data.data != None:
             print packet.data.data.data.data
